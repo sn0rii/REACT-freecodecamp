72f2f93af48baf7dd88f9f2ce0ac268d05534381
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-07 09:35:31 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -33,7 +33,14 @@ const Image = () => (
 const Title = () => {
   return <h2>Happy Place</h2>;
 };
-const Author = () => <h4>Emily Henry </h4>;
+const Author = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>Emily Henry </h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

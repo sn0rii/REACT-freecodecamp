1985d1556a277db575b3db49dc53eaf69f29fbd6
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-07 09:46:29 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -14,32 +14,19 @@ function BookList() {
   );
 }
 
+const author = "Emily Henry ";
 const Book = () => {
+  const title = "Happy Place";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg"
+        alt="Happy Place"
+      />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg"
-    alt="Happy Place"
-  />
-);
-const Title = () => {
-  return <h2>Happy Place</h2>;
-};
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>Emily Henry </h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

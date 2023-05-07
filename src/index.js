b5d7af4df20fd33ee504e83981f83a07360c0a36
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-07 12:01:56 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -30,13 +30,12 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };

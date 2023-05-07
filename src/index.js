f1db105483e6151d62cfff4408c1835150f154c5
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-07 10:02:29 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,29 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+const author = "Emily Henry ";
+const title = "Happy Place";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg";
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
     </section>
   );
 }
 
-const author = "Emily Henry ";
-const Book = () => {
-  const title = "Happy Place";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg"
-        alt="Happy Place"
-      />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };

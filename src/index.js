36040dffea65396d8e7e4d36a6edecb7b9d5acca
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-07 10:13:13 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const author = "Emily Henry ";
-const title = "Happy Place";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg";
+const firstBook = {
+  author: "Emily Henry",
+  title: "Happy Place",
+  img: "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg",
+};
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 }

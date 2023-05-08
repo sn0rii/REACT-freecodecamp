73d57cd0aff6982e2a7e70c3fb236c1499621c5c
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-08 07:23:17 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,30 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const firstBook = {
-  author: "Emily Henry",
-  title: "Happy Place",
-  img: "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg",
-};
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
-};
+const books = [
+  {
+    author: "Emily Henry",
+    title: "Happy Place",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
 }

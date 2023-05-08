a3c3a722c9d917d94e6c35920dbc47da8899c21d
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-08 09:49:30 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -18,26 +18,30 @@ const books = [
 ];
 
 function BookList() {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props;
-
-  const displayTitle = () => {
-    console.log(title);
+  const { img, title, author, getBook, id } = props;
+  const getSingleBook = () => {
+    getBook(id);
   };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={getSingleBook}>Get Book</button>
       <h4>{author}</h4>
     </article>
   );

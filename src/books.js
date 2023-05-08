cea99e14728c44aca84314ec8eb24109236f4c44
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-08 18:18:26 +0200

    Add files via upload

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: "Emily Henry",
+    title: "Happy Place",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,50 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const books = [
-  {
-    author: "Emily Henry",
-    title: "Happy Place",
-    img: "https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  const getSingleBook = () => {
-    getBook(id);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>Get Book</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
